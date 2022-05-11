223f97df5109c7d394a79f17652e4c54425d1987
lukaj110 <jovanovic.l01@htlwienwest.at>
2022-05-11 14:34:29 +0200

    Add project files.

---
+++ WebAPI/Controllers/GroupController.cs
@@ -0,0 +1,25 @@
+﻿using DataManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly WhisperContext _context;
+
+        public GroupController()
+        {
+            _context = new WhisperContext();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Group>> GetUsers()
+        {
+            return Ok(_context.Group.Include(e => e.User).ToList());
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using DataManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly WhisperContext _context;
+
+        public UserController()
+        {
+            _context = new WhisperContext();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return Ok(_context.User.Include(e => e.OwnerGroup).Include(e => e.Group).ToList());
+        }
+    }
+}
