a63e9f57e1ced496fdaa5805db0bc13bc3d43225
lukaj110 <jovanovic.l01@htlwienwest.at>
2022-05-12 00:01:03 +0200

    client: materialdesigninxaml shenanigans

---
+++ WhisperClient/LandingWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WhisperClient
+{
+    /// <summary>
+    /// Interaction logic for LandingWindow.xaml
+    /// </summary>
+    public partial class LandingWindow : Window
+    {    
+        public LandingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Card_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => this.DragMove();
+    }
+}
---
+++ WhisperClient/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WhisperClient.Pages
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Page
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Pages/LoginPage.xaml", UriKind.Relative));
+        }
+    }
+}
