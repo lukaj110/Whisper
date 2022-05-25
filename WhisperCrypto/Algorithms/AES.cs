23f94cc4814654611b5e9cad6948d5c64b2de9f5
lukaj110 <jovanovic.l01@htlwienwest.at>
2022-05-25 14:36:16 +0200

    started AES todo: diffie hellman

---
+++ WhisperCrypto/Algorithms/AES.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace WhisperCrypto.Algorithms
+{
+    internal class AES256
+    {
+        private readonly Aes aes;
+
+        public AES256()
+        {
+            aes = Aes.Create();
+
+            aes.BlockSize = 256;
+
+            aes.KeySize = 256;
+
+            aes.Mode = CipherMode.CBC;
+        }
+
+        public string Encrypt(string message, byte[] iv)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            byte[] aesEncryptedMessage = aes.EncryptCbc(messageBytes, iv);
+
+            return Convert.ToBase64String(aesEncryptedMessage);
+        }
+
+        public string Decrypt(string encryptedMessage, byte[] iv)
+        {
+            byte[] messageBytes = Convert.FromBase64String(encryptedMessage);
+
+            byte[] aesDecryptedMessage = aes.DecryptCbc(messageBytes, iv);
+
+            return Encoding.UTF8.GetString(aesDecryptedMessage);
+        }
+    }
+}
